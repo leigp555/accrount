5e96fbaea9ee76a855e095d8598e15cbeb6ff06e
leigp <907090585@qq.com>
2022-03-01 15:58:13 +0800

    初始化home页面

---
+++ src/components/RecordCalc.vue
@@ -0,0 +1,15 @@
+<template>
+calc
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "RecordCalc"
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/RecordTime.vue
@@ -0,0 +1,15 @@
+<template>
+time
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "RecordTime"
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
