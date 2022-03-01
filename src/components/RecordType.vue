b10c9205a46ec49504c397634f72bb096168f8cc
leigp <907090585@qq.com>
2022-03-01 19:24:56 +0800

    引入element-plus

--- src/components/RecordType.vue
+++ src/components/RecordType.vue
@@ -1,15 +1,26 @@
 <template>
-type
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane label="User" name="first">User</el-tab-pane>
+    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  </el-tabs>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-<script lang="ts">
-import {defineComponent} from "vue";
+const activeName = ref('first')
 
-export default defineComponent({
-  name: "RecordType"
-})
+const handleClick = (tab: string, event: Event) => {
+  console.log(tab, event)
+}
 </script>
-
-<style lang="scss" scoped>
-
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  background-color: #f4f5f7;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
 </style>
