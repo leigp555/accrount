e16ae1cc307d43b8d551275f48d17b6940bb8cbf
leigp <907090585@qq.com>
2022-03-02 03:32:24 +0800

    初始化home-recordTime组件

--- src/components/RecordNode.vue
+++ src/components/RecordNode.vue
@@ -1,5 +1,10 @@
 <template>
-node
+<div class="home-node-wrap">
+  <label>
+    <span>备注：</span>
+    <input type="text" placeholder="备注一下吧...">
+  </label>
+</div>
 </template>
 
 <script lang="ts">
@@ -11,5 +16,15 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-
+.home-node-wrap{
+  margin-top: 20px;
+  padding: 10px;
+  display: inline-flex;
+  align-items: center;
+  >label{
+    >input{
+      border: none;
+    }
+  }
+}
 </style>
--- src/components/RecordTime.vue
+++ src/components/RecordTime.vue
@@ -1,15 +1,29 @@
 <template>
-time
+  <div class="home-node-wrap">
+    <label>
+      <span>记账日期：</span>
+      <input type="datetime-local" value="2018-06-12T19:30">
+    </label>
+  </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 
 export default defineComponent({
-  name: "RecordTime"
+  name: "RecordNode"
 })
 </script>
 
 <style lang="scss" scoped>
-
+.home-node-wrap{
+  padding: 10px;
+  display: inline-flex;
+  align-items: center;
+  >label{
+    >input{
+      border: none;
+    }
+  }
+}
 </style>
