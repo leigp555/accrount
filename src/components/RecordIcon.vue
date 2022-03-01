0e1fa3ad84eca8ee3e802ad5cf30a1d0055815dd
leigp <907090585@qq.com>
2022-03-02 03:14:36 +0800

    完成home-recordIcon组件

--- src/components/RecordIcon.vue
+++ src/components/RecordIcon.vue
@@ -1,15 +1,116 @@
 <template>
-recordIcon
+  <div class="icon-wrap">
+    <div class="inner">
+      <div class="svgList" :class="{expenditureRight:countType==='expenditure'}">
+        <svg class="icon" v-for="(item,index) in incomeList" :key="index">
+          <use :xlink:href="`#icon-${item}`"></use>
+        </svg>
+      </div>
+      <div class="svgList xxx"
+           :class="{
+            expenditureLeft:countType==='expenditure',
+            expenditure:countType!=='expenditure'
+      }">
+        <svg class="icon" v-for="(item,index) in expenditureList" :key="index">
+          <use :xlink:href="`#icon-${item}`"></use>
+        </svg>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {computed, defineComponent} from "vue";
+import {useStore} from "vuex";
 
 export default defineComponent({
-  name: "RecordIcon"
+  name: "RecordIcon",
+  setup() {
+    const store = useStore()
+    const countType = computed(() => {
+      return store.state.countType
+    })
+    type list = { [key: number]: string }
+    const incomeList: list = {}
+    const expenditureList: list = {}
+    const incomeListContent = () => {
+      for (let i = 1; i < 12; i++) {
+        incomeList[i] = `income${i}`
+      }
+    }
+    const expenditureListContent = () => {
+      for (let i = 1; i < 17; i++) {
+        expenditureList[i] = `icon${i}`
+      }
+    }
+    incomeListContent()
+    expenditureListContent()
+    return {incomeList, expenditureList, countType}
+  }
 })
 </script>
 
 <style lang="scss" scoped>
+.icon-wrap {
+  width: 100%;
+  margin-top: 20px;
+  min-height: 30vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  border-bottom: 1px solid black;
 
+  > .inner {
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    width: 87vw;
+    margin-right: auto;
+    margin-left: auto;
+    position: relative;
+    top: 0;
+    left: 0;
+
+    > .svgList {
+
+      margin-right: -3vw;
+      transition: all 250ms;
+      display: inline-block;
+
+      &.expenditureLeft {
+        transform: translateX(0);
+      }
+
+      &.expenditureRight {
+
+        transform: translateX(-100vw);
+      }
+
+      &.expenditure {
+        transform: translateX(100vw);
+      }
+
+      &.xxx {
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+
+      > svg {
+        margin-right: 3vw;
+        margin-bottom: 25px;
+        clip-path: circle(50%);
+        background-color: rgba(255, 205, 67,.5);
+      }
+    }
+
+  }
+}
+
+.icon {
+  width: 12vw;
+  height: 12vw;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
