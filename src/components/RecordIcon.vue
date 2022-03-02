725e643eb9251f8fa56f9c12f767acfa9fa6c9d5
leigp <907090585@qq.com>
2022-03-02 23:01:52 +0800

    修复点击后的样式

--- src/components/RecordIcon.vue
+++ src/components/RecordIcon.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="icon-wrap">
     <div class="inner">
+
       <div class="svgList" :class="{expenditureRight:countType==='expenditure'}">
-        <svg class="icon" v-for="(item,index) in incomeList" :key="index" @click="toggle($event,item)">
-          <use :xlink:href="`#icon-${item}`"></use>
-        </svg>
+        <div class="svgWrap" v-for="(item,index) in incomeList" :key="index" @click="toggle($event,item)">
+          <svg class="icon">
+            <use :xlink:href="`#icon-${item}`"></use>
+          </svg>
+        </div>
+
       </div>
       <div class="svgList nextList"
-           :class="{
-            expenditureLeft:countType==='expenditure',
-            expenditure:countType!=='expenditure'
-      }">
-        <svg class="icon" v-for="(item,index) in expenditureList" :key="index" @click="toggle($event,item)">
-          <use :xlink:href="`#icon-${item}`"></use>
-        </svg>
+           :class="{expenditureLeft:countType==='expenditure',expenditure:countType!=='expenditure'}">
+
+        <div class="svgWrap" v-for="(item,index) in expenditureList" :key="index" @click="toggle($event,item)">
+          <svg class="icon">
+            <use :xlink:href="`#icon-${item}`"></use>
+          </svg>
+        </div>
+
+
       </div>
     </div>
   </div>
@@ -27,7 +33,7 @@ export default defineComponent({
   name: "RecordIcon",
   setup() {
     const store = useStore()
-    const selectedIcon=ref<HTMLDivElement>()
+    const selectedIcon = ref<HTMLDivElement>()
     const countType = computed(() => {
       return store.state.countType
     })
@@ -46,16 +52,16 @@ export default defineComponent({
     }
     incomeListContent()
     expenditureListContent()
-    watchEffect(()=>{
-      if(!store.state.iconNumber&&selectedIcon.value)selectedIcon.value!.classList.remove("selectedIcon")
+    watchEffect(() => {
+      if (!store.state.iconNumber && selectedIcon.value) selectedIcon.value!.classList.remove("selectedIcon")
     })
-    const toggle=(e:Event,iconNumber:string)=>{
-      store.commit("modifyIconNumber",iconNumber)
-      if(selectedIcon.value)selectedIcon.value!.classList.remove("selectedIcon")
-      selectedIcon.value =e.currentTarget as HTMLDivElement
+    const toggle = (e: Event, iconNumber: string) => {
+      store.commit("modifyIconNumber", iconNumber)
+      if (selectedIcon.value) selectedIcon.value!.classList.remove("selectedIcon")
+      selectedIcon.value = e.currentTarget as HTMLDivElement
       selectedIcon.value.classList.add("selectedIcon")
     }
-    return {incomeList, expenditureList, countType,toggle}
+    return {incomeList, expenditureList, countType, toggle}
   }
 })
 </script>
@@ -81,9 +87,8 @@ export default defineComponent({
     left: 0;
 
     > .svgList {
-      >.selectedIcon{
-        background-color: green;
-      }
+
+
       margin-right: -3vw;
       transition: all 250ms;
       display: inline-block;
@@ -107,11 +112,19 @@ export default defineComponent({
         left: 0;
       }
 
-      > svg {
+      > .svgWrap {
+        display: inline-flex;
         margin-right: 3vw;
         margin-bottom: 25px;
         clip-path: circle(50%);
-        background-color: rgba(255, 205, 67,.5);
+        background-color: rgba(255, 205, 67, .5);
+        width: 12vw;
+        height: 12vw;
+        justify-content: center;
+        align-items: center;
+      }
+      > .selectedIcon {
+        background-color: green;
       }
     }
 
@@ -119,8 +132,8 @@ export default defineComponent({
 }
 
 .icon {
-  width: 12vw;
-  height: 12vw;
+  width: 10vw;
+  height: 10vw;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
