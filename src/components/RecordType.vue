0078da7ab38d7a40b818f563f0ca1b87ef24b094
leigp <907090585@qq.com>
2022-03-02 17:34:30 +0800

    优化recordType,tab组件

--- src/components/RecordType.vue
+++ src/components/RecordType.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="typeComponent">
-    <Tabs v-model:selected="selected">
+    <Tabs >
       <Tab title="支出"></Tab>
       <Tab title="收入"></Tab>
     </Tabs>
@@ -9,22 +9,8 @@
 
 
 <script lang="ts" setup>
-import {useStore} from "vuex";
-
-const store = useStore()
 import Tab from "./lib/Tab.vue"
 import Tabs from "./lib/Tabs.vue"
-import {computed, ref, watchEffect} from "vue";
-
-const selected=ref("支出")
-
-watchEffect(() => {
-  if (selected.value === "收入") {
-    store.commit("modifyCountType", "income")
-  } else if (selected.value === "支出") {
-    store.commit("modifyCountType", "expenditure")
-  }
-})
 </script>
 
 <style lang="scss" scoped>
--- src/components/lib/Tabs.vue
+++ src/components/lib/Tabs.vue
@@ -3,84 +3,99 @@
     <div class="titles">
       <div class="inner">
         <div class="title" id="typeTitles" ref="wrap">
-          <div v-for="(item,index) in titles" :key="index" @click="toggle(item)">{{item}}</div>
+          <div v-for="(item,index) in titles" :key="index" @click="toggle(item)">{{ item }}</div>
         </div>
         <div class="indicator" ref="indicator"></div>
       </div>
     </div>
     <div class="content">
-      <component :is="currentContent" :key="currentContent" />
+      <component :is="currentContent" :key="currentContent"/>
     </div>
   </div>
 
 </template>
 <script lang="ts">
 import {computed, defineComponent, onMounted, ref, toRefs, watchEffect} from "vue";
+import {useStore} from "vuex";
+
 export default defineComponent({
-  props:{
-    selected:String
-  },
-  setup(props,ctx){
-    const selectedTitle=ref<string>(props.selected!)
-    const currentTitleEl=ref<HTMLDivElement>()
-    const children=ctx.slots.default!()
-    const currentContent=ref<HTMLDivElement>()
-    const indicator=ref<HTMLDivElement>()
-    const wrap=ref<HTMLDivElement>()
-    const titles=computed(()=>{
-      const arr=[] as string[]
-      children.forEach((item)=>{arr.push(item.props!.title)})
+  setup(props, ctx) {
+    const store = useStore()
+    const selectedTitle = ref("支出")
+    watchEffect(() => {
+      if (store.state.countType === "income") {
+       selectedTitle.value= "收入"
+      } else if (store.state.countType === "expenditure") {
+        selectedTitle.value= "支出"
+      }
+    })
+    const currentTitleEl = ref<HTMLDivElement>()
+    const children = ctx.slots.default!()
+    const currentContent = ref<HTMLDivElement>()
+    const indicator = ref<HTMLDivElement>()
+    const wrap = ref<HTMLDivElement>()
+    const titles = computed(() => {
+      const arr = [] as string[]
+      children.forEach((item) => {
+        arr.push(item.props!.title)
+      })
       return arr as string[]
     })
-    const toggle=(title:string)=>{
-      selectedTitle.value=title
-      ctx.emit("update:selected",title)
+    const toggle = (title: string) => {
+      if (title === "收入") {
+        store.commit("modifyCountType", "income")
+      } else if (title === "支出") {
+        store.commit("modifyCountType", "expenditure")
+      }
     }
-    onMounted(()=>{
-      const els=document.getElementById("typeTitles")!.children
-      const elements=Array.from(els)
-      const {left:left1}=wrap.value!.getBoundingClientRect()
-      watchEffect(()=>{
-        elements.forEach((item)=>{
-          if(item.innerHTML===selectedTitle.value){
+    onMounted(() => {
+      const els = document.getElementById("typeTitles")!.children
+      const elements = Array.from(els)
+      const {left: left1} = wrap.value!.getBoundingClientRect()
+      watchEffect(() => {
+        elements.forEach((item) => {
+          if (item.innerHTML === selectedTitle.value) {
             //@ts-ignore
-            currentTitleEl.value=item
-            const {width,left:left2}=item.getBoundingClientRect()
-            indicator.value!.style.width=width+"px"
-            const distance=left2-left1
-            indicator.value!.style.transform=`translateX(${distance+'px'})`
+            currentTitleEl.value = item
+            const {width, left: left2} = item.getBoundingClientRect()
+            indicator.value!.style.width = width + "px"
+            const distance = left2 - left1
+            indicator.value!.style.transform = `translateX(${distance + 'px'})`
           }
         })
-        children.forEach((item)=>{
-          if(item.props!.title===selectedTitle.value){
+        children.forEach((item) => {
+          if (item.props!.title === selectedTitle.value) {
             //@ts-ignore
-            currentContent.value=item
+            currentContent.value = item
           }
         })
       })
     })
-    return {titles,toggle,currentContent,indicator,wrap}
+    return {titles, toggle, currentContent, indicator, wrap}
   }
 })
 </script>
 
 <style lang="scss" scoped>
-.type-wrap{
-  >.titles{
+.type-wrap {
+  > .titles {
     padding: 10px;
     display: flex;
     justify-content: center;
-    >.inner{
+
+    > .inner {
       position: relative;
       top: 0;
       left: 0;
-      >.title{
+
+      > .title {
         display: flex;
         gap: 50px;
         margin-bottom: 3px;
         font-size: 18px;
       }
-      >.indicator{
+
+      > .indicator {
         position: absolute;
         bottom: 0;
         left: 0;
