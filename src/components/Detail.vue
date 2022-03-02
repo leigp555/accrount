28eecd1799ea5dada8748c4ec9d367b606547467
leigp <907090585@qq.com>
2022-03-02 23:02:25 +0800

    初始化detail组件

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,13 +1,22 @@
 <template>
-detail
+<div class="detail-wrap">
+  <div class="nav"></div>
+  <div class="content"></div>
+</div>
 </template>
 
-<script lang="ts">
-import {defineComponent} from "vue";
+<script lang="ts" setup>
+import {useStore} from "vuex";
+import {computed, watchEffect} from "vue";
 
-export default defineComponent({
-  name: "Detail"
+const store=useStore()
+store.commit("fetchData")
+const allDate=computed(()=>{
+  return store.state.fetchDate
 })
+
+//{"countType": "expenditure","iconNumber": "icon1","node": "ok","nodeTime": "2022-03-02T18:55:54","countMoney": 78}
+
 </script>
 
 <style lang="scss" scoped>
